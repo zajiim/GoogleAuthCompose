8d98c9baca9944ebc81dde63c558d4599d897515
sajims95 <sajims95@gmail.com>
2023-12-12 14:04:40 +0530

    Introduced Edge to Edge for status bar and navigation bar color...

--- app/src/main/java/com/example/googleauthcomposelearning/MainActivity.kt
+++ app/src/main/java/com/example/googleauthcomposelearning/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.googleauthcomposelearning
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.enableEdgeToEdge
 import com.example.googleauthcomposelearning.navigation.NavGraph
-import com.example.googleauthcomposelearning.ui.theme.GoogleAuthComposeLearningTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -19,6 +14,16 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
         setContent {
             NavGraph()
         }
